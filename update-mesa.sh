055dc407f1c13656220cc36031aa959296c8c603
Deemoxzj <127976173+Deemoxzj@users.noreply.github.com>
2024-11-23 13:23:58 +0800

    Update update-mesa.sh

--- update-mesa.sh
+++ update-mesa.sh
@@ -8,16 +8,17 @@ sudo apt install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf -y
 git clone https://gitlab.freedesktop.org/mesa/mesa.git mesa-sources
 #
 echo '[binaries]
-c = 'arm-linux-gnueabihf-gcc'
-cpp = 'arm-linux-gnueabihf-g++'
-ar = 'arm-linux-gnueabihf-ar'
-strip = 'arm-linux-gnueabihf-strip'
-pkgconfig = 'arm-linux-gnueabihf-pkg-config'
+c = "arm-linux-gnueabihf-gcc"
+cpp = "arm-linux-gnueabihf-g++"
+ar = "arm-linux-gnueabihf-ar"
+strip = "arm-linux-gnueabihf-strip"
+pkgconfig = "arm-linux-gnueabihf-pkg-config"
+
 [host_machine]
-system = 'linux'
-cpu_family = 'arm'
-cpu = 'arm'
-endian = 'little''>> ~/cross_armhf.txt
+system = "linux"
+cpu_family = "arm"
+cpu = "arm"
+endian = "little"' > ~/cross_armhf.txt
 #
 cd mesa-sources
 meson b32 --cross-file=~/cross_armhf.txt --libdir=lib/arm-linux-gnueabihf -Dgallium-drivers=freedreno,zink,virgl,d3d12,softpipe  -Dvulkan-drivers=freedreno -Dgallium-nine=false -Dfreedreno-kmds=kgsl,msm -Dglx=dri -Dxlib-lease=enabled -Dplatforms=x11,wayland -Ddri3=enabled -Dvulkan-beta=true -Dvideo-codecs=all -Dglx-direct=true -Dtools=drm-shim,freedreno -Dopengl=true -Dosmesa=true -Dgles1=enabled -Dgles2=enabled -Degl-native-platform=x11 -Degl=enabled -Dpower8=enabled -Dgallium-d3d12-video=enabled -Dgallium-d3d12-graphics=enabled  -Dxmlconfig=enabled -Dgbm=enabled --prefix=~/mesa/
