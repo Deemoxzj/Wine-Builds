5a1afc7b9b17eb8f4e8589c4bd44518aff48c3d5
Deemoxzj <127976173+Deemoxzj@users.noreply.github.com>
2024-11-23 14:53:09 +0800

    Update update-mesa.sh

--- update-mesa.sh
+++ update-mesa.sh
@@ -1,6 +1,8 @@
 cd ~
 sudo sed -i 's/jammy/oracular/g' /etc/apt/sources.list
 sudo sed -i 's/jammy/oracular/g' /etc/apt/sources.list.d/ubuntu.sources
+sudo sed -i 's/deb/deb [arch=amd64,armhf]/g' /etc/apt/sources.list
+sudo sed -i 's/deb/deb [arch=amd64,armhf]/g' /etc/apt/sources.list.d/ubuntu.sources
 sudo apt update
 sudo apt full-upgrade
 sudo apt install git meson -y 
@@ -23,4 +25,4 @@ cpu = 'arm'
 endian = 'little'' > ~/cross_armhf.txt
 #
 cd mesa-sources
-meson b32 --cross-file=~/cross_armhf.txt --libdir=lib/arm-linux-gnueabihf -Dgallium-drivers=freedreno,zink,virgl,d3d12,softpipe  -Dvulkan-drivers=freedreno -Dgallium-nine=false -Dfreedreno-kmds=kgsl,msm -Dglx=dri -Dxlib-lease=enabled -Dplatforms=x11,wayland -Ddri3=enabled -Dvulkan-beta=true -Dvideo-codecs=all -Dglx-direct=true -Dtools=drm-shim,freedreno -Dopengl=true -Dosmesa=true -Dgles1=enabled -Dgles2=enabled -Degl-native-platform=x11 -Degl=enabled -Dpower8=enabled -Dgallium-d3d12-video=enabled -Dgallium-d3d12-graphics=enabled  -Dxmlconfig=enabled -Dgbm=enabled --prefix=~/mesa/
+meson b32 --cross-file=~/cross_armhf.txt --libdir=lib/arm-linux-gnueabihf -Dgallium-drivers=freedreno,zink,virgl,d3d12,softpipe  -Dvulkan-drivers=freedreno -Dgallium-nine=false -Dfreedreno-kmds=kgsl,msm -Dglx=dri -Dxlib-lease=enabled -Dplatforms=x11,wayland -Dvulkan-beta=true -Dvideo-codecs=all -Dglx-direct=true -Dtools=drm-shim,freedreno -Dopengl=true -Dosmesa=true -Dgles1=enabled -Dgles2=enabled -Degl-native-platform=x11 -Degl=enabled -Dpower8=enabled -Dgallium-d3d12-video=enabled -Dgallium-d3d12-graphics=enabled  -Dxmlconfig=enabled -Dgbm=enabled --prefix=~/mesa/
