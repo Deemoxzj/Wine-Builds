5d7de908e751c37857e07f41d37ee8bd1f7ad398
Deemoxzj <127976173+Deemoxzj@users.noreply.github.com>
2024-11-24 19:52:43 +0800

    Update wow64_only64.sh

--- wow64_only64.sh
+++ wow64_only64.sh
@@ -338,8 +338,8 @@ export CROSSCXXFLAGS="${CROSSCFLAGS_X64}"
 
 sudo apt-get install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
 sudo apt-get install mingw-w64
-wget -O ${BUILD_DIR}"/ https://github.com/bylaws/llvm-mingw/releases/download/20240929/llvm-mingw-20240929-ucrt-ubuntu-20.04-x86_64.tar.xz
-tar -xvf llvm-mingw-20240929-ucrt-ubuntu-20.04-x86_64.tar.xz
+wget -O "${BUILD_DIR}"/ https://github.com/bylaws/llvm-mingw/releases/download/20240929/llvm-mingw-20240929-ucrt-ubuntu-20.04-x86_64.tar.xz
+tar -xvf llvm-mingw-20240929-ucrt-ubuntu-20.04-x86_64.tar.xz "${BUILD_DIR}"/
 export ${BUILD_DIR}/llvm-mingw-20240929-ucrt-ubuntu-20.04-aarch64/bin:$PATH
 mkdir "${BUILD_DIR}"/build64
 cd "${BUILD_DIR}"/build64 || exit
