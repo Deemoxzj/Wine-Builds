79d78189ca5eebbc79fd92a54ba5dacc237381de
Deemoxzj <127976173+Deemoxzj@users.noreply.github.com>
2025-01-18 18:27:09 +0800

    Update dxvk-gplasync-master.sh

--- dxvk-gplasync-master.sh
+++ dxvk-gplasync-master.sh
@@ -5,6 +5,6 @@ git clone --recursive https://github.com/doitsujin/dxvk.git dxvk
 cp ~/dxvk-gplasync/patches/dxvk-gplasync-master.patch ~/dxvk
 cd dxvk
 patch -p 1 -i dxvk-gplasync-master.patch
-./package-release.sh master /opt/dxvk-gplasync --no-package
+./package-release.sh master /opt/dxvk-gplasync-master --no-package
 cd /opt/
-tar -cjvf dxvk-gplasync-master.tar.gz dxvk-gplasync
+tar -cjvf dxvk-gplasync-master.tar.xz dxvk-gplasync-master
